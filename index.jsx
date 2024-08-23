9989b8bed31b53108d0df459b555c76444a4a923
MONICA <monik.ferrer@gmail.com>
2024-08-23 15:07:40 +0200

    First commit

---
+++ index.jsx
@@ -0,0 +1,21 @@
+// Element with children
+//
+//              opening tag         children
+//              |  attributes       |        closing tag
+//              |  |                |        |
+const element = <p className="text">Some Text</p>;
+//               | |         |                 |
+//               | |         attribute value   |
+//               | attribute name              |
+//               tag name or component type ---+
+
+// Self-closing element
+//
+//            self closing tag   slash denotes self closing
+//            |      attributes  |
+//            |      |           |
+const input = <input type="text" />;
+//             |     |    |
+//             |     |    attribute value
+//             |     attribute name
+//             tag name or component type
